efe97d5da3a1024ba5a21f524c555502ebf7597f
Flor Cerdan <cerdanflor@gmail.com>
2022-07-18 12:13:14 -0500

    Semana 14 - Iniciando con filtros

--- ProyectosEnDJango/proyecto001/miapp/views.py
+++ ProyectosEnDJango/proyecto001/miapp/views.py
@@ -23,16 +23,35 @@ layout = """
 """
 
 def index(request):
-    
-    return render(request,'index.html')
-    
+    estudiantes = ['Carlos Ruiz',
+                   'Jordy Quispe',
+                   'Oscar Reyes',
+                   'Antony Vasquez']
+    estudiantes = []
+    return render(request,'index.html', {
+        'titulo':'Inicio',
+        'mensaje':'Proyecto Web con Django en LP3',
+        'estudiantes': estudiantes
+    })
+
+
 def saludo(request):
     
-    return render(request,'saludo.html')
+    return render(request,'saludo.html' ,{
+        'titulo': 'Saludo',
+        'autor_saludo':'Mg. Flor Elizabeth Cerdán León'
+    })
 
 def rango(request):
     a = 10
     b= 20
+    rango_numeros = range(a,b+1)
+    return render(request, 'rango.html',{
+        'titulo':'Rango',
+        'a': a,
+        'b':b,
+        'rango_numeros': rango_numeros
+    })
     resultado = f"""
              <h2> Numeros de [{a},{b}] </h2>
              resultado: <br>
